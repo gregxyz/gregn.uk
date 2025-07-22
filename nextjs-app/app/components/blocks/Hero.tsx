e84b47897d81d787d3659666f924e043b444cc4f
Greg Nicholson <greg@gregn.uk>
2025-07-22 21:59:58 +0100

    feat: implement GSAP animations in Hero component and enhance text styles

--- nextjs-app/app/components/blocks/Hero.tsx
+++ nextjs-app/app/components/blocks/Hero.tsx
@@ -1,39 +1,70 @@
 "use client";
 
 import type { Hero as HeroProps } from "@/sanity.types";
+import { useGSAP } from "@gsap/react";
 import { ArrowElbowRightDown } from "@phosphor-icons/react";
+import gsap from "gsap";
 import Image from "next/image";
+import { useRef } from "react";
+import { AnimateFadeUp } from "../common/AnimateFadeUp";
 import RichText from "../common/RichText";
 
 function Hero({ block }: { block: HeroProps }) {
+  const overlayRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    if (!overlayRef.current) return;
+
+    const overlay = overlayRef.current;
+
+    gsap.to(overlay, {
+      width: 0,
+      duration: 1.2,
+      delay: 0.3,
+      ease: "expo.out",
+    });
+  });
+
   return (
     <section className="relative flex h-dvh flex-col bg-grid px-6 py-10 md:px-20 md:py-16">
-      <div className="mb-5 grid-cols-2 md:mb-0 md:grid">
+      <div className="mb-8 grid-cols-2 md:mb-0 md:grid">
         <div className="relative z-20 mb-4 md:mb-0">
-          <h1 className="whitespace-pre-line font-bold text-fluid-xl">
-            {block.title}
-          </h1>
+          <AnimateFadeUp delay={0.2}>
+            <h1 className="whitespace-pre-line font-bold text-fluid-xl">
+              {block.title}
+            </h1>
+          </AnimateFadeUp>
         </div>
         <div className="max-w-[400px] md:ml-auto md:place-self-end md:text-right xl:max-w-[30vw]">
-          {block.description && <RichText content={block.description} />}
+          {block.description && (
+            <AnimateFadeUp delay={0.6}>
+              <RichText content={block.description} />
+            </AnimateFadeUp>
+          )}
           {!!block.links && (
-            <nav>
-              <hr className="mt-2 mb-1 w-7 md:ml-auto" />
-              <ul className="inline-flex justify-end space-x-2 pr-1 text-xs underline opacity-50">
-                {block.links.map((link) => (
-                  <li key={link._key}>
-                    <a href={link.url} target="_blank" rel="noreferrer">
-                      {link.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </nav>
+            <AnimateFadeUp delay={0.65}>
+              <nav>
+                <hr className="mt-2 mb-1 w-7 md:ml-auto" />
+                <ul className="inline-flex justify-end space-x-2 pr-1 text-xs underline opacity-50">
+                  {block.links.map((link) => (
+                    <li key={link._key}>
+                      <a href={link.url} target="_blank" rel="noreferrer">
+                        {link.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </AnimateFadeUp>
           )}
         </div>
       </div>
       <div className="relative z-10 flex h-full flex-col md:flex-row md:justify-between">
         <div className="md:-ml-10 md:-mt-15 lg:-mt-6 relative size-full min-w-[200px] shrink-0 overflow-hidden md:h-[75vh] md:w-[18vw]">
+          <div
+            ref={overlayRef}
+            className="absolute right-0 z-20 size-full bg-white"
+          />
           <Image
             src="/images/hero-demo.jpg"
             alt=""
@@ -54,9 +85,11 @@ function Hero({ block }: { block: HeroProps }) {
           </button>
         </div>
         <div className="@container hidden w-full justify-end self-end md:flex">
-          <h2 className="whitespace-pre-line text-right font-bold text-[16cqw] leading-[16cqw] tracking-[0.5vw]">
-            {block.titleLarge}
-          </h2>
+          <AnimateFadeUp delay={0.35}>
+            <h2 className="whitespace-pre-line text-right font-bold text-[16cqw] leading-[14cqw] tracking-[0.5vw]">
+              {block.titleLarge}
+            </h2>
+          </AnimateFadeUp>
         </div>
       </div>
       <div className="pointer-events-none absolute inset-x-0 bottom-0 z-5 h-[120px] bg-linear-to-t from-white to-transparent" />
