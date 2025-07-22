bc52b364c0d4c8bc225dc96cbf608370d755a7d2
Greg Nicholson <greg@gregn.uk>
2025-07-22 22:00:19 +0100

    style: update Footer component layout and text alignment for improved responsiveness

--- nextjs-app/app/components/core/Footer.tsx
+++ nextjs-app/app/components/core/Footer.tsx
@@ -2,10 +2,10 @@ function Footer() {
   return (
     <footer className="mt-20 bg-[#000] px-10 pt-5 pb-10">
       <div className="border-white/10 border-t pt-10">
-        <ul className="flex w-full justify-between font-thin text-white/40 text-xs uppercase tracking-widest">
+        <ul className="grid w-full grid-cols-2 justify-between gap-x-2 gap-y-10 font-thin text-white/40 text-xs uppercase tracking-widest sm:flex sm:flex-row sm:gap-0">
           <li>Greg Nicholson.</li>
-          <li>Senior Developer.</li>
-          <li>Based in London.</li>
+          <li className="text-right sm:text-left">Senior Developer.</li>
+          <li className="col-span-2 text-center">Based in London.</li>
         </ul>
       </div>
     </footer>
