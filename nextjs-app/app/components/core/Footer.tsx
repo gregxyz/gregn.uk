308ea5e3b46803f2516e05722cce86341bd342eb
Greg Nicholson <greg@gregn.uk>
2025-07-30 23:21:10 +0100

    fix: correct font weight in Footer component and adjust line height for text fluidity

--- nextjs-app/app/components/core/Footer.tsx
+++ nextjs-app/app/components/core/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="mt-20 bg-[#000] px-10 pt-5 pb-10">
       <div className="border-white/10 border-t pt-10">
-        <ul className="grid w-full grid-cols-2 justify-between gap-x-2 gap-y-10 font-thin text-white/40 text-xs uppercase tracking-widest sm:flex sm:flex-row sm:gap-0">
+        <ul className="grid w-full grid-cols-2 justify-between gap-x-2 gap-y-10 font-light text-white/40 text-xs uppercase tracking-widest sm:flex sm:flex-row sm:gap-0">
           <li>Greg Nicholson.</li>
           <li className="text-right sm:text-left">Senior Developer.</li>
           <li className="col-span-2 text-center">Based in London.</li>
